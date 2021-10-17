afcd999f83ef5dc0a85a77c1c6f7ecb8408ce71f
Narlier Oliveira <narlier.oliveira@gmail.com>
2021-10-17 18:59:49 -0300

    Crição dos templates e ajustes na rota URL.

--- agenda/urls.py
+++ agenda/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core import views
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos, name='agenda'),
+    path('', RedirectView.as_view(url='/agenda/'))
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
+from django.http import response
 from django.shortcuts import render
 
+from .models import Evento
+
+
 # Create your views here.
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    context = {'eventos': evento}
+    return render(request, 'core/agenda.html', context)
